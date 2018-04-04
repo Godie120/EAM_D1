7f8f13f5b72266e058d64d41dd058ab714621c95
Godie <godie120@hotmail.com>
2018-04-04 17:51:16 -0500

    Init version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'My First App Angular!';
+	author = 'Diego Fernando';
+	count = 0;
+	imgSource = './../favicon.ico';
+	enabled = true;
 }
